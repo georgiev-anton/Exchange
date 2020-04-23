2ca33b8fed214563bccefe40ad8ebd0168ea8415
Антон Георгиев <antongeorgiev@MacBook-Pro-Anton.local>
2020-04-24 04:05:25 +0500

    Правый блок

--- src/components/rightBody/RightBody.js
+++ src/components/rightBody/RightBody.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { updateTabsRightBody } from "../../redux/tabs/actions";
+import { ReactComponent as PlusInElipse } from "./assets/plusElipse.svg";
 
 const RightBody = ({ tabs, updateTabsRightBody }) => {
   return (
@@ -19,7 +20,29 @@ const RightBody = ({ tabs, updateTabsRightBody }) => {
           Продать BTC
         </p>
       </div>
-      <div>{tabs === "BUY" ? <p>BUY</p> : <p>SELL</p>}</div>
+      <div className="rightBodyDeposit p-2">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex flex-column align-items-start mr-5">
+            <p className="mb-0">Доступно: USDT</p>
+            <p className="mb-0">2.00023011</p>
+          </div>
+          <div>
+            <p>
+              <PlusInElipse className="plusInElipse" /> Депозит
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <span>Лимит</span>
+        <div>
+          <span>Цена</span> <input /> <span>USDT</span>
+        </div>
+        <div>
+          <span>Количество</span> <input /> <span>BTC</span>
+        </div>
+      </div>
+      {tabs === "BUY" ? <p>BUY</p> : <p>SELL</p>}
     </div>
   );
 };
