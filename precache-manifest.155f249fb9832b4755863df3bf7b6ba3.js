2836b976b0201072778451ef94d8ac555a209250
Антон Георгиев <antongeorgiev@MacBook-Pro-Anton.local>
2020-04-23 16:50:37 +0500

    Updates

---
+++ precache-manifest.155f249fb9832b4755863df3bf7b6ba3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb57ff70d42cf7ea4b51f63e336f6b0f",
+    "url": "/Exchange/index.html"
+  },
+  {
+    "revision": "d467cb00fef60131e765",
+    "url": "/Exchange/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "682d7889be508532ed3a",
+    "url": "/Exchange/static/css/main.470db2ac.chunk.css"
+  },
+  {
+    "revision": "d467cb00fef60131e765",
+    "url": "/Exchange/static/js/2.ae574492.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/Exchange/static/js/2.ae574492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682d7889be508532ed3a",
+    "url": "/Exchange/static/js/main.08ee3f51.chunk.js"
+  },
+  {
+    "revision": "547626f3ff93487096ae",
+    "url": "/Exchange/static/js/runtime-main.52bbfe65.js"
+  },
+  {
+    "revision": "a3d7d652af07c3610c453b4a51c788c7",
+    "url": "/Exchange/static/media/GothamPro-Bold.a3d7d652.woff"
+  },
+  {
+    "revision": "c15ee62b232cedc240947b6d814fb750",
+    "url": "/Exchange/static/media/GothamPro-Bold.c15ee62b.ttf"
+  },
+  {
+    "revision": "ee9b8c537d217fef51d4bca8cf436d7d",
+    "url": "/Exchange/static/media/GothamPro-Bold.ee9b8c53.eot"
+  },
+  {
+    "revision": "6fe466da15644ccc370d3b2ce3a19f6f",
+    "url": "/Exchange/static/media/GothamPro-Medium.6fe466da.eot"
+  },
+  {
+    "revision": "b86e995a10856a8b8e222fe8ff00a74f",
+    "url": "/Exchange/static/media/GothamPro-Medium.b86e995a.woff"
+  },
+  {
+    "revision": "c161369173f822acd66d2ff8eea64c52",
+    "url": "/Exchange/static/media/GothamPro-Medium.c1613691.ttf"
+  },
+  {
+    "revision": "16b3d5d1925dfaa26da12a1ec8b87790",
+    "url": "/Exchange/static/media/earth.16b3d5d1.svg"
+  },
+  {
+    "revision": "ba568d25579082353fd5cb3143ce3873",
+    "url": "/Exchange/static/media/logo.ba568d25.svg"
+  },
+  {
+    "revision": "60c91d095aed25f37bfea2c8020afb8a",
+    "url": "/Exchange/static/media/settings.60c91d09.svg"
+  }
+]);
